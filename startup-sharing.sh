d8cf2db6df74e5e3a51ea52ca43d0cdfefc5a3d2
Julien Loudet <julien@cozycloud.cc>
2016-01-05 15:39:44 +0100

    Updates / fixes
    * Printed some information for the user to display what is going on
    * Separated the code dealing with adding the ssh key
    * Updated the way the git repositories are updated: I get the name of the
    remote branch the current branch is tracking (if it exists), I then check
    if the local branch is behind the remote ; if so I start a git pull - and I
    warn the user so that (s)he can fix the conflicts if there are any.
    If there is no local branch that matches the name of the branch we are
    looking for I check the remote branches to see if there is one that
    contains said name. If so I create a local branch that is set to track
    the remote selected and I pull.
    
    There are still, at least, the following "problems":
    * if several remote names match I don't know what happens (haha)
    * I don't ask the user if (s)he is fine with pulling from the remote: in my
    case I assume that what is on the remote is tested and does not break
    anything (in short it's safe to pull)
    * when I create the local branch I assume the name of the remote follows
    the pattern "origin/branch_name" (that is a BIG ASSUMPTION)

--- startup-sharing.sh
+++ startup-sharing.sh
@@ -3,34 +3,50 @@
 # Debug purposes: echo the command before issuing it
 # set -x
 
+# Sources:
+# https://stackoverflow.com/questions/171550/find-out-which-remote-branch-a-local-branch-is-tracking
+
 readonly BASEDIR="$HOME/dev/PhD"
 readonly VM1="$BASEDIR/cozy-vm1"
 readonly VM2="$BASEDIR/cozy-vm2"
 
 
 load_modules() {
+    echo "[STARTUP] Loading modules."
+
     local mods="vboxdrv vboxnetadp vboxnetflt"
     for module in $mods
     do
-        if ! (lsmod | grep $module &> /dev/null)
+        if !(lsmod | grep $module &> /dev/null)
         then
             echo "[SUDO] Loading: $module"
             sudo modprobe $module
         fi
     done
+
+    echo "[STARTUP] Loading modules: done."
 }
 
 
-update_git_repositories() {
+load_ssh_key() {
+    echo "[STARTUP] Adding ssh key."
+
     # Add the ssh key linked to my github account
     local ssh_key="$HOME/.ssh/a-julien--github"
     # The "ssh-add -l" list all the ssh keys that are actually loaded ; we then
     # grep the results to check if the ssh key linked to my account is among
     # them
-    if ! (ssh-add -l | grep $ssh_key &> /dev/null)
+    if !(ssh-add -l | grep $ssh_key &> /dev/null)
     then
         ssh-add $ssh_key
     fi
+
+    echo "[STARTUP] Adding ssh key: done."
+}
+
+
+update_git_repositories() {
+    echo "[STARTUP] Updating repositories."
 
     # Go through each dir of both virtual machines and update the git
     # repositories if a branch sharing exists
@@ -45,11 +61,52 @@ update_git_repositories() {
             then
                 cd $dir
 
-                # check that the current dir is indeed a git repository and
-                # has a branch sharing
-                if [ -d .git ] && (git rev-parse --verify $branch &> /dev/null)
+                # First case: the dir is a git repository and has a local
+                # branch that has the name specified
+                if (git rev-parse --verify $branch &> /dev/null)
                 then
-                    git checkout $branch
+
+                    local current_branch=$(git symbolic-ref --short -q HEAD)
+                    local remote_branch=$(git branch -r | \
+                        grep $branch 2> /dev/null)
+
+                    # Switch the repository to the branch we are interested in
+                    if [[ $current_branch != $branch ]]
+                    then
+                        echo "[INFO] $vm/$dir: switching to $branch"
+                        git checkout $branch
+                    fi
+
+                    local symbolic_ref=$(git symbolic-ref -q HEAD)
+                    local remote_tracked_branch=$(git for-each-ref \
+                        --format='%(upstream:short)' $symbolic_ref)
+
+                    # If there is a remote
+                    if [[ ! -z $remote_tracked_branch ]]
+                    then
+                        # First update the remote information
+                        git remote update &> /dev/null
+                        # Then get the number of commits that are behind the
+                        # remote
+                        local nb_commit=$(git rev-list \
+                            $branch..$remote_tracked_branch --count)
+
+                        # If that number is greater than 0 then we can pull
+                        if [[ $nb_commit -gt 0 ]]
+                        then
+                            echo "\n\n[INFO] Repository $vm/$dir: pull"
+                            git pull
+                        fi
+                    fi
+
+                # Second case: there is no local branch but there is a remote
+                # branch that has the same name. We create a local branch that
+                # will follow this particular remote.
+                elif [[ ! -z $remote_branch ]]
+                then
+                    echo "[INFO] $vm/$dir: creating branch $branch"
+                    git checkout -b $branch
+                    git branch --set-upstream-to=origin/$branch $branch
                     git pull
                 fi
 
@@ -60,10 +117,13 @@ update_git_repositories() {
         # we don't forget to go back to the parent directory of the vm
         cd $BASEDIR
     done
+
+    echo "[STARTUP] Updating repositories: done."
 }
 
 main() {
     load_modules
+    load_ssh_key
     update_git_repositories
 }
 
